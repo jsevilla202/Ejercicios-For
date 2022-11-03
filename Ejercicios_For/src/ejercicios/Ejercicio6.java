9c8ee5c7e79dbecbe4e73799270fa147dfb9ef9a
jsevilla <jsevilla@106-19.info2>
2022-11-03 14:42:09 +0100

    Ejercicios For

--- Ejercicios_For/src/ejercicios/Ejercicio6.java
+++ Ejercicios_For/src/ejercicios/Ejercicio6.java
@@ -1,18 +1,20 @@
 package ejercicios;
 
-public class Ejercicio2 {
-
+public class Ejercicio6 {
 	public static void main(String[] args) {
+		int n = 0, suma = 0;
 		/*
 		 * En el bucle "for":
 		 * - Inicializamos la variable "i" a 2 ( ya que es el primero número par ) y la nombramos como número entero
 		 * - No se parará hasta llegar al número 200
 		 * - Suma 2 a la variable "i" cada vuelta
 		 */
-		for(int i=2; i <= 200; i=i+2) {
-			//Mostramos los números pares que hay en el rango
-			System.out.println(i);
+		for(int i=0; n <= 10; i++) {
+			if(i%2!=0) {
+				n++;
+				suma = suma +i;
+			}
 		}
+		System.out.println("La suma total de los 10 primeros números impares es: "+suma);
 	}
-
 }
